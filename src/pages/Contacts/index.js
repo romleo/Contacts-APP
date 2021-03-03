c5b7abc253c3903113ac966323427bcab0136359
Roman <romandybinetc@gmail.com>
2021-03-03 12:34:40 +0200

    add hooks

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,29 @@
+import {useState ,useEffect} from 'recat';
+
 export const Contacts = () =>{
-    return <div>Contacts</div>
+    const [contacts, setContacts] = useState ([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+       
+    useEffect(() =>{
+        setIsLoading(trye);
+        fetch("https://randomuser.me/api/?results=200")
+        .then(response =>response.json())
+        .then((results )=>{
+            setContacts(results);
+            setIsLoading(false);
+            setIsError(false);
+        }).catch(() =>{
+            setIsLoading(false)
+           setIsError(true);
+        })      
+    }, []);
+
+    if (isLoading) {
+        return<div>...LOADING</div>;
+    }
+    if (isError) {
+        return<div>...ERROR</div>;
+    }
+    return <div>Contacts {contacts[0].name.first}</div>;
 }
