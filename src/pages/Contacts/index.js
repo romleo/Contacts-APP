17370e3ad970fa482e90cabe3bc29a42939f4a8c
Roman <romandybinetc@gmail.com>
2021-03-04 12:44:58 +0200

    create castom useContacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,7 +1,30 @@
 import { useState, useEffect } from 'recat';
 
-const useContacts = () =>{
-    
+const useContacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        const getContacts = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch("https://randomuser.me/api/?results=200");
+                const { results, error } = await response.json();
+                if (error) {
+                    throw new Error(error);
+                }
+                setContacts(results);
+                setIsError(false);
+            } catch (e) {
+                setIsError(true);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        };
+        getContacts()
+    }, []);
 }
 
 
