7016b73b56545f2d228870dbb263e8a2b2127490
Roman <romandybinetc@gmail.com>
2021-03-04 12:43:12 +0200

    refactoring contacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,29 +1,53 @@
-import {useState ,useEffect} from 'recat';
+import { useState, useEffect } from 'recat';
 
-export const Contacts = () =>{
-    const [contacts, setContacts] = useState ([]);
-    const [isLoading, setIsLoading] = useState(false);
+const useContacts = () =>{
+    
+}
+
+
+export const Contacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
-       
-    useEffect(() =>{
-        setIsLoading(trye);
-        fetch("https://randomuser.me/api/?results=200")
-        .then(response =>response.json())
-        .then((results )=>{
-            setContacts(results);
-            setIsLoading(false);
-            setIsError(false);
-        }).catch(() =>{
-            setIsLoading(false)
-           setIsError(true);
-        })      
+
+    useEffect(() => {
+        const getContacts = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch("https://randomuser.me/api/?results=200");
+                const { results, error } = await response.json();
+                if (error) {
+                    throw new Error(error);
+                }
+                setContacts(results);
+                setIsError(false);
+            } catch (e) {
+                setIsError(true);
+            }
+            finally {
+                setIsLoading(false);
+            }
+
+            // fetch("https://randomuser.me/api/?results=200")
+            //     .then(response => response.json())
+            //     .then((results) => {
+            //         setContacts(results);
+            //         setIsLoading(false);
+            //         setIsError(false);
+            //     })
+            //     .catch(() => {
+            //         setIsLoading(false)
+            //         setIsError(true);
+            //     });//refactoring code 
+        };
+        getContacts()
     }, []);
 
     if (isLoading) {
-        return<div>...LOADING</div>;
+        return <div>...LOADING</div>;
     }
     if (isError) {
-        return<div>...ERROR</div>;
+        return <div>...ERROR</div>;
     }
     return <div>Contacts {contacts[0].name.first}</div>;
 }
